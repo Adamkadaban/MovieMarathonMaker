0e11968cbc8bbc531dd8af48879dc33f132a5432
mhayworth <73895101+mhayworth@users.noreply.github.com>
2021-12-04 15:59:51 -0500

    Update Graph.cpp
    added comments, small fix on paths vector updating

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,17 +3,18 @@
 #include "Graph.h"
 
 pair<string, double> getMin(map<string, vector<pair<string, double>>>& paths, unordered_set<int> avail) {
-    pair<string, double> currentShortest = ("NULL", INT_MAX);
-    map<string, double> totalRunLength;
+    //returns the movie that has not been found yet with the shortest path to it
+    pair<string, double> currentShortest = ("NULL", INT_MAX); //shortest movie
+    map<string, double> totalRunLength; //total run length of all movies in a given movie's path
     for (auto iter : paths) {
-        totalRunLength[iter.first] = 0;
+        totalRunLength[iter.first] = 0; //initialization
         for (int i = 0; i < iter.second.size(); i++) {
-            totalRunLength[iter.first] += iter.second;
+            totalRunLength[iter.first] += iter.second; 
         }
     }
     for (auto iter : totalRunLength) {
-        if (iter.second < currentShortest.second && avail.count(iter.first) == 1) {
-            currentShortest.first = iter.first;
+        if (iter.second < currentShortest.second && avail.count(iter.first) == 1) { //if the current total run length is shorter than the saved run length...
+            currentShortest.first = iter.first;   //make the shortest total run length the current
             currentShortest.second = iter.second;
         }
     }
@@ -49,6 +50,7 @@ map<string, pair<string, double>> Graph::getAdjacent(string vertex) {
 }
 
 double getSum(vector<pair<string, double>>& v) {
+    //used to find the current length of movies in a vector
     double sum = 0;
     for (int i = 0; i < v.size(); i++) {
         sum += v.second;
@@ -57,12 +59,12 @@ double getSum(vector<pair<string, double>>& v) {
 }
 
 map<string, vector<pair<string, double>>> Graph::dijkstra(string vertex) {
-    map<string, vector<pair<string, double>>> paths;
-    map<string, string> parents;
+    map<string, vector<pair<string, double>>> paths; //read as map<actor, vector<movie>>
+    map<string, string> parents; 
     unordered_set<string> completed;
     unordered_set<string> notCompleted;
     for (auto iter : adjList) {
-        iter.second.push_back(make_pair("NULL", INT_MAX));
+        paths[iter.first]push_back(make_pair("NULL", INT_MAX)); //insert starting value 
         notCompleted.insert(iter.first);
     }
     
@@ -76,8 +78,10 @@ map<string, vector<pair<string, double>>> Graph::dijkstra(string vertex) {
             if (sum(paths[index.first]) + iter.second.second < sum(paths[iter.first])) {
                 if (paths[iter.first].at(0).second == INT_MAX) {
                     paths[iter.first] = paths[index.first];
+                    paths[iter.first].push_back(iter.second);
                 }
                 else {
+                    paths[iter.first].insert(end(paths[iter.first]), begin(paths[index.first]), end(paths[index.first]));
                     paths[iter.first].push_back(iter.second);
                 }
                 parents[iter.first] = index.first;
@@ -90,7 +94,7 @@ map<string, vector<pair<string, double>>> Graph::dijkstra(string vertex) {
             cout << "this shouldn't happen";
             break;
         }
-        completed.insert(index);
+        completed.insert(index.first);
     }
     return paths;
 }
